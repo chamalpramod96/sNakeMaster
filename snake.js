57e508deff8cc782a48a96c31de4bfe721f70a0e
chamalpramod96 <chamalps21@gmail.com>
2021-04-04 13:42:47 +0530

    add increas value and death functions

--- snake.js
+++ snake.js
@@ -3,6 +3,19 @@ function Snake(){
 	this.y = 0;
 	this.xspeed = 1;
 	this.yspeed = 0;
+	this.total= 0;
+	this.tail= [];
+
+
+	this.eat = function(pos){
+		var d = dist(this.x, this.y, pos.x, pos.y);
+		if(d<1){
+			this.total++;
+			return true;
+		}else{
+			return false;
+		}
+	}
 
   this.dir = function(x, y){
     this.xspeed = x;
@@ -10,8 +23,22 @@ function Snake(){
   }
 	
 	this.update = function(){
-		this.x = this.x + this.xspeed+scl;
-		this.y = this.y + this.yspeed+scl;
+
+		if(this.total === this.tail.length){
+			for(var i =0; i< this.tail.length-1; i++){
+				this.tail[i] = this.tail[i+1];
+			}
+		}
+		this.tail[this.total-1] = createVector(this.x, this.y);	
+
+		for(var i=0; i< this.total-1; i++){
+			this.tail[i] = this.tail[i+1];
+		}
+
+		this.tail[this.total-1] = createVector(this.x, this.y);
+
+		this.x = this.x + this.xspeed*scl;
+		this.y = this.y + this.yspeed*scl;
 
     this.x = constrain(this.x,0,width-scl);
     this.y = constrain(this.y,0,height-scl);
@@ -19,7 +46,21 @@ function Snake(){
 
 	this.show = function(){
 		fill(255);
-		rect(this.x , this.y, 10 , 10)
+		for(var i=0;i<this.tail.length;i++){
+			rect( this.tail[i].x , this.tail[i].y, scl , scl);
+		}
+		rect(this.x , this.y, scl , scl);
+	}
+
+	this.death =function(){
+		for(var i=0; i< this.tail.length; i++){
+			var pos = this.tail[i];
+			var d = dist(this.x, this.y, pos.x, pos.y);
+			if(d<1){
+				this.total = 0;
+				this.tail = [];
+			}
+		}		
 	}
 
 }
